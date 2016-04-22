9891e003ef54eafc9b6d4d15247c3b22079b49da
Rasmus <r.ovlesen@gmail.com>
2016-04-22 21:51:11 +0200

    Added a UnitController and a new unit prefab (Not sure of units will be made as prefabs or setup scripts yet...)

---
+++ Team Survival/Assets/Scripts/Movement/UnitController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BaseMotor))]
+public class UnitController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BaseMotor motor = GetComponent<BaseMotor>();
+        motor.Body.GetComponent<Renderer>().material.color = Color.red;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
