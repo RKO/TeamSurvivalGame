b9c5da1ad0a42c8a930a732db02a28c3a6065c55
Rasmus <r.ovlesen@gmail.com>
2016-09-24 16:12:52 +0200

    Basic attack can now "hit" targets (only prints their name currently)

--- Team Survival/Assets/Scripts/Abilities/AbilityBasicAttack.cs
+++ Team Survival/Assets/Scripts/Abilities/AbilityBasicAttack.cs
@@ -2,7 +2,10 @@
 
 public class AbilityBasicAttack : BaseAbility
 {
-    private const float Cooldown = 0.5f;
+    private const float Cooldown = 1f;
+    private const float MaxDistance = 0.5f;
+    private Vector3 HalfBox = new Vector3(1f, 1f, 0.5f);
+    private LayerMask targetMask = 1 << LayerMask.NameToLayer("Unit");
 
     public override string Name { get { return "Attack"; } }
 
@@ -11,5 +14,28 @@ public class AbilityBasicAttack : BaseAbility
     protected override void DoActivate()
     {
         _animSync.RpcTriggerAnimation(UnitTriggerAnimation.Attack1);
+
+        Vector3 inFront = _caster.transform.position + _caster.transform.forward + Vector3.up;
+
+        RaycastHit[] hits = Physics.BoxCastAll(inFront, HalfBox, _caster.transform.forward, _caster.transform.rotation, MaxDistance, targetMask);
+        foreach (var hit in hits)
+        {
+            Debug.DrawLine(_caster.transform.position + Vector3.up, hit.collider.transform.position + Vector3.up, Color.yellow);
+            
+
+            BaseUnit unit = hit.transform.gameObject.GetComponentInChildren<BaseUnit>();
+            if (unit != null)
+            {
+                DoHitOnTarget(unit);
+            }
+            else {
+                Debug.LogError("Collider on \"" + hit.transform.gameObject.name+"\" in \"Unit\" layer does not have a BaseUnit component!");
+            }
+        }
+    }
+
+    private void DoHitOnTarget(BaseUnit target) {
+        if(target.GetTeam == Team.Enemies)
+            Debug.Log("Hit: " + target.Name);
     }
 }
