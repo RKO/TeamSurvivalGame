6b1e96e29bff0552af09c500610add9ad4cedf53
Rasmus <r.ovlesen@gmail.com>
2016-12-10 20:04:44 +0100

    Fixed player animation synch on clients.

--- Team Survival/Assets/Scripts/Movement/Characters/WarriorPrincessCharacter.cs
+++ Team Survival/Assets/Scripts/Movement/Characters/WarriorPrincessCharacter.cs
@@ -27,6 +27,8 @@ public class WarriorPrincessCharacter : BaseUnit
 
     // Update is called once per frame
     void Update () {
+        if (!IsOnServer)
+            return;
 
         bool idle = false;
         bool combat = true;
