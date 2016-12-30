6273525e3a6f6ccc7d2c1c9d5469cecf535f9e13
Rasmus <r.ovlesen@gmail.com>
2016-12-30 15:21:06 +0100

    Use interface instead of implementation.

--- Team Survival/Assets/Scripts/Abilities/AbilityBasicAttack.cs
+++ Team Survival/Assets/Scripts/Abilities/AbilityBasicAttack.cs
@@ -13,7 +13,7 @@ public class AbilityBasicAttack : BaseAbility
 
     public override string Name { get { return "Attack"; } }
 
-    public AbilityBasicAttack(BaseMotor caster, BaseUnit unit) : base(caster, unit, Cooldown, Duration) {
+    public AbilityBasicAttack(IMotor caster, BaseUnit unit) : base(caster, unit, Cooldown, Duration) {
         _hitTable = new Dictionary<Transform, UnitShell>();
     }
 
@@ -25,12 +25,13 @@ public class AbilityBasicAttack : BaseAbility
 
     protected override void AbilityUpdate()
     {
-        Vector3 inFront = _caster.transform.position + _caster.transform.forward + Vector3.up;
+        Transform transform = _unit.transform;
+        Vector3 inFront = transform.position + transform.forward + Vector3.up;
 
-        RaycastHit[] hits = Physics.BoxCastAll(inFront, HalfBox, _caster.transform.forward, _caster.transform.rotation, MaxDistance, targetMask);
+        RaycastHit[] hits = Physics.BoxCastAll(inFront, HalfBox, transform.forward, transform.rotation, MaxDistance, targetMask);
         foreach (var hit in hits)
         {
-            Debug.DrawLine(_caster.transform.position + Vector3.up, hit.collider.transform.position + Vector3.up, Color.yellow);
+            Debug.DrawLine(transform.position + Vector3.up, hit.collider.transform.position + Vector3.up, Color.yellow);
 
             if (!_hitTable.ContainsKey(hit.transform)) {
                 UnitShell unit = hit.transform.gameObject.GetComponentInChildren<UnitShell>();
