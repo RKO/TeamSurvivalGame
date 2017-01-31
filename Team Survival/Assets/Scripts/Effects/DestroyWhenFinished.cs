f1e4fcf4ecdd804444b877a67eefacf246610706
Rasmus <r.ovlesen@gmail.com>
2017-01-31 21:29:55 +0100

    Adding this file... again?

---
+++ Team Survival/Assets/Scripts/Effects/DestroyWhenFinished.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyWhenFinished : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ParticleSystem particle = GetComponent<ParticleSystem>();
+
+        StartCoroutine(DelayedDestroy(particle.duration));
+    }
+
+    private IEnumerator DelayedDestroy(float time) {
+        yield return new WaitForSeconds(time);
+
+        Destroy(this.gameObject);
+    }
+}
