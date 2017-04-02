8559204255fc9f17d99d3ec5b94097ea713fea88
Rasmus <r.ovlesen@gmail.com>
2017-04-02 11:54:24 +0200

    Created AbilityInfo system and a registry.

---
+++ Team Survival/Assets/Scripts/Network/Synchronization/AbilityInfoSync.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AbilityInfoSync : MonoBehaviour {
+
+    [SerializeField]
+    private List<AbilityInfo> RegisteredAbilities;
+
+    private static Dictionary<string, AbilityInfo> _abilityMap;
+
+    private void Awake()
+    {
+        _abilityMap = new Dictionary<string, AbilityInfo>();
+
+        foreach (var ability in RegisteredAbilities)
+        {
+            _abilityMap.Add(ability.UniqueID, ability);
+        }
+    }
+
+    public static AbilityInfo GetAbilityInfo(string uniqueID) {
+        if (!_abilityMap.ContainsKey(uniqueID))
+            throw new KeyNotFoundException("No AbilityInfo registered for ID: \""+uniqueID+"\".");
+
+        return _abilityMap[uniqueID];
+    }
+}
