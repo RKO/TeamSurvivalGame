12f6dce9aae62df26e7a384cba658a8889ffb658
Rasmus <r.ovlesen@gmail.com>
2017-07-03 20:34:49 +0200

    Added a script that prevents the camera from going through walls. (Camera view may still clip through, but camera position will not)

---
+++ Team Survival/Assets/Scripts/Movement/CameraClippingController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraClippingController : MonoBehaviour
+{
+    private static LayerMask TerrainLayerMask = 1 << 8;
+
+    [SerializeField]
+    private Transform CameraTrans;
+
+    private Vector3 _originalCameraPosition;
+    private float _originalDistance;
+
+	private void Start () {
+        _originalCameraPosition = CameraTrans.localPosition;
+        _originalDistance = Vector3.Distance(transform.localPosition, _originalCameraPosition);
+    }
+	
+	private void Update () {
+
+        Vector3 dir = transform.TransformPoint(_originalCameraPosition) - transform.position;
+
+        Ray ray = new Ray(transform.position, dir);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, _originalDistance, TerrainLayerMask))
+        {
+            CameraTrans.localPosition = transform.InverseTransformPoint(hit.point);
+        }
+        else {
+            CameraTrans.localPosition = _originalCameraPosition;
+        }
+	}
+}
